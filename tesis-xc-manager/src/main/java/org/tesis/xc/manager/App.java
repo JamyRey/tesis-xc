912a4ad4539832575f3a097c76e5f0277a10b52b
Jamy <pasantejamy@gmail.com>
2023-10-31 23:08:45 -0400

    Inicializado completado

---
+++ tesis-xc-manager/src/main/java/org/tesis/xc/manager/App.java
@@ -0,0 +1,13 @@
+package org.tesis.xc.manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
